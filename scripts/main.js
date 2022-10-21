90adf5b58594d04cfef6eb3912b6907879a446fb
Jessica Meeker <jesmeeker@gmail.com>
2022-10-21 12:02:29 -0400

    parkAreas.js html function

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,7 @@
 import { getAttractions, getParkAreas, getServices, getGuests } from "./database.js";
 import { currentGuestsList } from "./guests.js";
+import { parkAreasList } from "./parkAreas.js";
 
-// let attractions = getAttractions()
-// let parkAreas = getParkAreas()
-// let services = getServices()
-// let guests = getGuests()
-
-// console.log(attractions)
-// console.log(parkAreas)
-// console.log(services)
-// console.log(guests)
 
 const mainContainer = document.querySelector("#container")
 
@@ -18,25 +10,17 @@ const applicationHTML = `
 <article class="details">
     <section class="detail--column list details__cities">
         <h2>Park Areas</h2>
-        ${getParkAreas()}}
+        ${parkAreasList()}
     </section>
-    <section class="detail--column list details__cities">
-        <h2>Park Services</h2>
-        ${getServices()}
-    </section>
-    <section class="detail--column list details__cities">
-        <h2>Attractions</h2>
-        ${getAttractions()}
+
     </section><section class="detail--column list details__cities">
     <h2>Guests</h2>
         ${currentGuestsList()}
 </section>
 </article>
 
-<article class="assignments">
-    <h2>Current Assignments</h2>
-   
-</article>
+
 `
 
 mainContainer.innerHTML = applicationHTML
+
--- scripts/parkAreas.js
+++ scripts/parkAreas.js
@@ -1,5 +1,30 @@
-import { getParkAreas, getAttractions, getServices } from "./database";
+import { getParkAreas, getAttractions, getServices } from "./database.js";
 
 let areas = getParkAreas()
 let attractions = getAttractions()
 let services = getServices()
+
+//export and define a function that creats an HTML String
+//Park Area Name followed by Attractions in the Area and Service Tags
+
+export const parkAreasList = () => {
+    let parkAreasHTML = ""
+    
+    for (const area of areas) {
+        parkAreasHTML += `<h3>${area.name}</h3>`
+        parkAreasHTML += `<h4>Attractions</h4>`
+
+        for (const attraction of attractions) {
+            if (area.id === attraction.parkAreasId) {
+                parkAreasHTML += `<div>${attraction.name}</div>`
+                for (const service of services) {
+
+                    if (service.attractionsId === attraction.id)
+                    
+                    parkAreasHTML += `<li>${service.name}</li>`
+                }
+            }   
+        }
+    }
+    return parkAreasHTML
+}
